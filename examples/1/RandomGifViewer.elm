b4eca13a7b067e2ef86136f75a524c2200c6f681
Andre Pitombeira <andrepitombeira@gmail.com>
2015-10-15 15:02:38 +0200

    Create random gif viewer

---
+++ examples/1/RandomGifViewer.elm
@@ -0,0 +1,46 @@
+type alias Model =
+  { topic : String
+  , gifUrl : String
+  }
+
+init : String -> (Model, Effets Action)
+update : Action -> Model -> (Model, Effects Action)
+
+module Effects where
+
+type Effets a
+
+none : Effets a
+
+task : Task Never a -> Effects a
+
+type Action
+    = RequestMore
+    | NewGif (Maybe String)
+
+update : Action -> Model -> (Model, Effects Action)
+update msg model =
+  case msg of
+    RequestMore ->
+      ( model
+      , getRandomGif model.topic
+      )
+
+    NewGif mayberUrl ->
+      ( Model model.topic (Maybe.withDefault model.gifUrl mayber)
+      , Effects.none
+      )
+
+getRandomGif : String -> Effects Action
+getRandomGif topic =
+    Http.get decodeImageUrl (randomUrl topic)
+      |> Task.toMaybe
+      |> Task.map NewGif
+      |> Effets.task
+
+randomUrl: String -> String
+randomUrl topic =
+  Http.url
+
+decodeImageUrl : Json.Decoder String
+decodeImageUrl = Json.at ["data", "image_url"] Json.string
